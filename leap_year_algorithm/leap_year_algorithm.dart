102cf6f857fbc0a9ed1823ad09f9c5b5ef0bfbdf
iwase_mio <mio.disney110@gmail.com>
2024-02-06 15:08:49 +0900

    閏年アルゴリズムの作成

--- leap_year_algorithm/leap_year_algorithm.dart
+++ leap_year_algorithm/leap_year_algorithm.dart
@@ -1,3 +1,10 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+void main() {
+  int year = 2024;
+  if (year % 100 == 0 && year % 400 != 0) {
+    print('平年だあ');
+  } else if (year % 4 == 0) {
+    print('閏年だあ');
+  } else {
+    print('平年だあ');
+  }
 }
